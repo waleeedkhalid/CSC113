3e15e680432d89161f572e18b9959cb0d4298bf4
waleeedkhalid <axd.edits@gmail.com>
2024-02-01 16:07:57 +0300

    Revert "create two methods of addStudent uses Aggregation and Composition"
    This reverts commit 4e3c3bcdea33ec3e45bd97e32c5a893414a00a5c.

--- src/chapter_1/Main.java
+++ src/chapter_1/Main.java
@@ -11,14 +11,14 @@ public class Main {
     	
     	// Aggregation Add will be enter the reference and not creating new object just do the code =>
     	// array[counter] = stu;
-//    	sec.addStudentAggregation(sAgg);
+    	sec.addStudentAggregation(sAgg);
     	sAgg.setStudentAge(50);
     	System.out.println(sAgg.getStudentAge());
     	System.out.println(sec.Students[0].getStudentAge());
     	
     	// Composition will Create a new copy Student Object such if the section deleted the objects inside also will be destoryed
     	// array[counter] = new Student(stu);
-//    	sec.addStudentComposition(sComp);
+    	sec.addStudentComposition(sComp);
     	sComp.setStudentAge(50);
     	System.out.println(sComp.getStudentAge()); // 50
     	System.out.println(sec.Students[1].getStudentAge()); // 21 (won't changed because has another ref value)
